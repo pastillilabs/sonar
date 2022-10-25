01825b46e7d6ae0279e5f8ccbd5d25947f16eaea
nephros <nemo@pgxperiiia10>
2022-10-25 11:59:55 +0200

    avoid error from -Wunused-result

--- service/src/main.cpp
+++ service/src/main.cpp
@@ -12,9 +12,10 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
 
     const QVersionNumber productVersion{QVersionNumber::fromString(QSysInfo::productVersion())};
-    if(productVersion > QVersionNumber(3, 4) {
-        setenv("HOME", QDir::homePath(), 1);            // homePath() returns absolute path
-        setenv("USER", QDir::home().dirName(), 1);      // home.dirName() returns last path segment
+    const QVersionNumber checkVersion = QVersionNumber(3, 4);
+    if(productVersion > checkVersion) {
+        setenv("HOME", QDir::homePath().toLocal8Bit().constData(), 1);            // homePath() returns absolute path
+        setenv("USER", QDir::home().dirName().toLocal8Bit().constData(), 1);      // home.dirName() returns last path segment
     }
 
     int result(1);
